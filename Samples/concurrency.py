72725b27d13210d4861efef8d695b6d57ed7e9df
João Gabriel Lopes <jglopes6@gmail.com>
2019-06-06 17:34:00 -0300

    Renamed functions

--- Samples/concurrency.py
+++ Samples/concurrency.py
@@ -2,15 +2,15 @@ import asyncio
 import time
 
 
-async def thread1(message):
+async def userInterface(message):
     await asyncio.sleep(1)
     print(message)
 
-async def thread2(message):
+async def messageReciever(message):
     await asyncio.sleep(5)
     print(message)
 
-async def thread3(message):
+async def detectLeader(message):
     await asyncio.sleep(10)
     print(message)
 
@@ -18,9 +18,9 @@ async def main():
     print(f"started at {time.strftime('%X')}")
 
     await asyncio.gather( 
-        thread1("Hello from thread 1"),
-        thread2("Hello from thread 2"),
-        thread3("Hello from thread 3"),
+        userInterface("Hello from thread 1"),
+        messageReciever("Hello from thread 2"),
+        detectLeader("Hello from thread 3"),
     )
 
     print(f"finished at {time.strftime('%X')}")
