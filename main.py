775df3a3e239285f4eb86902eff37a8d3c1617be
João Gabriel Lopes <jglopes6@gmail.com>
2019-06-06 22:44:07 -0300

    Initial stage of connect protocol

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys, select
 import os
 
 
+program_list = []
 
 ################ USER INPUT HANDLERS ################
 
@@ -35,6 +36,20 @@ async def userInterfaceThread():
         if get_data() == 'leader':
             verifyLeader()
 
+async def connectReturn(writer):
+    global program_list
+    
+    message = str(program_list)
+
+    writer.write(message.encode())
+    await writer.drain()
+    
+    return message
+
+
+
+
+
 ################ CONNECTION HANDLERS ################
 
 ## Sending
@@ -62,9 +77,16 @@ async def serverFunc(reader, writer):
 
     print(f"Received {message!r} from {addr!r}")
 
-    print(f"Send: {message!r}")
-    writer.write(data)
-    await writer.drain()
+    # Splits the string into ints
+    messageElements = [int(n) for n in message.split("|")]
+    
+    if (messageElements[0] == 6):
+        message = await connectReturn(writer)
+        #print(f"Send: {message!r}")
+    else:
+        #print(f"Send: {message!r}")
+        writer.write(data)
+        await writer.drain()
 
     print("Close the connection")
     writer.close()
@@ -84,20 +106,42 @@ async def detectLeaderThread():
 
 
 async def main():
+    global program_list
+
+    ################# MESSAGE TABLE
+    ########## 1. ELEIÇÃO
+    ########## 2. OK
+    ########## 3. LIDER
+    ########## 4. VIVO
+    ########## 5. VIVO_OK
+    ########## 6. CONNECT (requisição para entrar na rede)
+
 
     # We use the process' PID as a unique identifier in multiple occastions on the program.
     # This means that all of the identifier, port, and value of election is equal to the ID.
 
     uniqueID = os.getpid()
-
-    program_list = []
-    connect_port = input("Insira o PID de um processo existente para se conectar: ")
-    program_list.append(int(connect_port))
+    isConnected = False
     
+
+    while (isConnected == False):
+        connect_port = int(input("Insira o ID de um processo existente para se conectar (-1 para se conectar a ninguém): "))
+        try:
+            if (connect_port != -1):
+                await sendMessage(f"6|{uniqueID}|{uniqueID}|000", connect_port)
+                isConnected = True
+            else:
+                isConnected = True
+        except ConnectionError:
+            print("Erro de conexão, tente novamente")
+    
+    if (connect_port != -1):
+        program_list.append(connect_port)
+
     # Starts a server (in order to receive TCP messages) on the port of the PID (always unique)
     server = await asyncio.start_server(serverFunc, '127.0.0.1', uniqueID)
     
-    print(f'Serving on {uniqueID}')
+    print(f'Seu ID é {uniqueID}')
     
     await asyncio.gather(
         userInterfaceThread(),
